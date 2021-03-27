92f24a224a67baf4d7e8c9acef5b9074f463b0b3
smetzdev <metz@raum3.de>
2021-03-27 17:34:43 +0100

    Add initial module headline

---
+++ libs/shared/ui/src/lib/elements/headline/headline.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Headline } from "./headline"
+
+export default {
+  title: "Headline",
+  component: Headline,
+}
+
+export const Base = () => <Headline>Module Headline</Headline>
---
+++ libs/shared/ui/src/lib/elements/headline/headline.tsx
@@ -0,0 +1,8 @@
+import tw, { styled } from "twin.macro"
+
+export const Headline = styled.h2`
+  ${tw`flex items-center justify-between gap-8 text-4xl`}
+  &::before,&::after {
+    ${tw`flex-1 h-px bg-gray-500 bg-repeat-x content`}
+  }
+`
