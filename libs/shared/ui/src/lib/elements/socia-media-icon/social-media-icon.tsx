8cc069866939d3971ef01043eee3bab72483ecc1
smetzdev <metz@raum3.de>
2021-04-24 17:42:22 +0200

    feat(libs/shared/ui/elements/social-media-icon): add js-doc comment

--- libs/shared/ui/src/lib/elements/socia-media-icon/social-media-icon.tsx
+++ libs/shared/ui/src/lib/elements/socia-media-icon/social-media-icon.tsx
@@ -1,6 +1,9 @@
 import { Icon } from "../icon"
 import { getIconName } from "./get-icon-name"
 
+/** Retuns a social-media icon based on the url
+ * if no icon could be found, it returns the "globe"-icon
+ */
 export const SocialMediaIcon = ({ url, ...props }: SocialMediaIconProps) => {
   const iconName = getIconName(url)
   return <Icon name={iconName} {...props} />
